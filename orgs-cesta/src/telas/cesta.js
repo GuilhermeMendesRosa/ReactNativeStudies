6cde76e5497df439675369c895f0fcaf42b107ca
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2023-05-27 22:43:02 -0300

    .

--- orgs-cesta/src/telas/cesta.js
+++ orgs-cesta/src/telas/cesta.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Image, StyleSheet, Dimensions, Text } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+
+import logo from '../../assets/logo.png';
 import topo from '../../assets/topo.png';
 
 export default function Cesta() {
@@ -7,6 +9,19 @@ export default function Cesta() {
         <>
             <Image source={topo} style={estilos.topo} />
             <Text style={estilos.titulo}>Detalhe da Sexta</Text>
+
+            <View style={estilos.cesta}>
+                <View style={estilos.viewTitulo} >
+                    <Image style={estilos.logo} source={logo} />
+                    <Text style={estilos.nome}>Cesta de Verduras</Text>
+                </View>
+                <Text style={estilos.nomeFazenda} >Jenny Jack Farm</Text>
+                <Text style={estilos.descricao}>Uma sexta com produtos selecionados
+                    cuidadosamente da fazenda direto
+                    para a cozinha
+                </Text>
+                <Text style={estilos.preco}>R$ 40,00</Text>
+            </View>
         </>)
 }
 
@@ -20,12 +35,48 @@ const estilos = StyleSheet.create({
     titulo: {
         width: '100%',
         position: 'absolute',
-        marginTop: 200,
+        marginTop: 50,
         textAlign: 'center',
         fontSize: 16,
         lineHeight: 16,
         color: 'white',
         fontWeight: 'bold',
         padding: 16
+    },
+    viewTitulo: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10,
+        alignItems: 'center'
+    },
+    logo: {
+        height: 30,
+        width: 30
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontWeight: "bold"
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26
+    },
+    descricao: {
+        color: "#a3a3a3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 })
